158280a68812d303993006e4b08a82ec58c876f4
cest <cestrmail@163.com>
2018-08-08 15:27:25 +0800

    # 兼容手机模式 # 图片懒加载

--- project/src/js/index.js
+++ project/src/js/index.js
@@ -1,10 +1,41 @@
-$(()=>{
+$(() => {
 
     /* ========================================================================= */
-	/*	Page Preloader
+    /*	Page Preloader
     /* ========================================================================= */
-    
-    window.onload = ()=>{
+
+    window.onload = () => {
         document.getElementById('loading-mask').style.display = 'none';
     }
+
+
+    /* ========================================================================= */
+    /*	myCarousel init
+    /* ========================================================================= */
+
+    $('#myCarousel').carousel({
+        //设置自动播放/3 秒
+        interval: 5000,
+    });
+
+    /* ========================================================================= */
+    /*	myCarousel 图片延迟加载
+    /* ========================================================================= */
+
+    function lazyContainer(searchNode) {
+        $(searchNode).find('.active').find('img.lazy').each(function() {
+            var imgSrc = $(this).attr('data-src');
+            if (imgSrc) {
+                $(this).attr('src', imgSrc);
+                $(this).attr('data-src', '');
+            }
+        });
+    }
+
+    $('#myCarousel').bind('slid.bs.carousel', function() {
+        lazyContainer(this);
+    });
+
+    lazyContainer('#myCarousel');
+
 });
